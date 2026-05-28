f7ec28dcd471cbf2598cf677b4e076c37ba3db9d
Mohit Malik <mohit2326m@gmail.com>
2026-05-28 14:51:31 +0530

    Create q7.kt

---
+++ q7.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    val a=3
+    val b=2
+    val c=5
+    if (a > b && a > c)
+    {
+        print("$a is a largest")
+    }
+    else if (b > c && b > a)
+    {
+        print("$b is a largest")
+    }
+    else
+    {
+        print("$c is a largest")
+    }
+}
